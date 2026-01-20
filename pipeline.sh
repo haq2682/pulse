dd51120ad5b34db149315be51c24194de8b451b5
haq2682 <haq2682@gmail.com>
2026-01-20 15:33:45 +0500

    Update pipeline script to correct mapping script path and ensure analysis step is included

--- pipeline.sh
+++ pipeline.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 docker cp ./mapping python:/app/
-./pydoc.sh mapping/map.py
+./pydoc.sh mapping/run_mapping.py
 
 docker cp ./cleaning python:/app/
 ./pydoc.sh cleaning/cleaning.py
 
 docker cp ./transformation python:/app/
 ./pydoc.sh transformation/transformation.py
+
+docker cp ./analysis python:/app/
+./pydoc.sh analysis/analysis.py
 
 read -p "Press Enter to exit..."
