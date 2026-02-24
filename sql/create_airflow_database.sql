397bd94d93c9014f4ecc76cabdaef822a454e307
haq2682 <haq2682@gmail.com>
2026-02-24 23:11:10 +0500

    Refactor Airflow database creation script by commenting out the conditional check

--- sql/create_airflow_database.sql
+++ sql/create_airflow_database.sql
@@ -3,5 +3,8 @@
 -- The 'airflow' DB is separate from the main 'pulse' application DB so that
 -- Airflow metadata does not interfere with application data.
 
-SELECT 'CREATE DATABASE airflow'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'airflow')\gexec
+-- SELECT 'CREATE DATABASE airflow'
+-- WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'airflow')\gexec
+
+-- ./sql/create_airflow_db.sql
+CREATE DATABASE airflow;
